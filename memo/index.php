0a4d1e2a0a87ebbf762a7af4575c7758899268f6
naokiowada <naoki.owada.709@gmail.com>
2022-02-25 16:49:12 +0900

    [Update] 次のページへ、前のページへのリンクを作成

--- memo/index.php
+++ memo/index.php
@@ -1,5 +1,11 @@
 <?php
 require('dbconnect.php');
+/*最大ページ数を求める*/
+$counts =$db->query('select count(*) as cnt from memos');
+$count = $counts->fetch_assoc();
+$max_page = floor(($count['cnt'])/5+1);
+echo $max_page;
+
 $stmt = $db->prepare('select * from memos order by id desc limit ?, 5');
 if (!$stmt) {
     die($db->error);
@@ -10,7 +16,6 @@ $start = ($page - 1) * 5;
 $stmt->bind_param('i', $start);
 // page=に不正な値が入力されたときの処理※教材が間違っているため動作しません
 $result = $stmt->execute();
-
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -35,6 +40,13 @@ $result = $stmt->execute();
     </div>
     <hr>
     <?php endwhile; ?>
-   
+    <p>
+        <?php if ($page>1): ?>
+        <a href="?page=<?php echo $page-1 ?>"><?php echo $page-1 ?> ページ目へ</a> |
+        <?php endif; ?>
+        <?php if ($page<$max_page): ?>
+        <a href="?page=<?php echo $page+1 ?>"><?php echo $page+1 ?> ページ目へ</a>
+        <?php endif; ?>
+    </p>
 </body>
 </html>
